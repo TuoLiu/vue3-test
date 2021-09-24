66187936ac23cfd4ac841b2a5b76e37eb4d63886
liutuo <610936147@qq.com>
2021-09-24 10:21:56 +0800

    add axios

---
+++ src/views/EventCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <h1>Events For Good</h1>
+    <div class="events">
+      <EventCard v-for="event in events" :event="event" v-bind:key="event.id" />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "../components/EventCard.vue"
+import EventService from "../services/EventService"
+
+export default {
+  name: "Home",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    }
+  },
+  created() {
+    // get events from mock db when component  is created
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
